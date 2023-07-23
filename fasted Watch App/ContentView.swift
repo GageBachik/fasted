aa18cd4068b8af257f23008d56f5baa8d9e2ee5e
Mikael Carpenter <mikaelcarp.dev@gmail.com>
2023-07-22 20:05:02 -0700

    Some basic UI and the ability to create a new Cycle on "Start"

--- fasted Watch App/ContentView.swift
+++ fasted Watch App/ContentView.swift
@@ -5,19 +5,49 @@
 //  Created by Gage Bachik on 7/22/23.
 //
 
+import CoreData
+import Foundation
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var timers: FetchedResults<Cycle>
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Cycle.startDate, ascending: false)]) var cycles: FetchedResults<Cycle>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 16) {
+            if let firstCycle = cycles.first, let startDate = firstCycle.startDate {
+                Text(startDate, style: .date)
+            } else {
+                Text("15 hours")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            
+            Button("Start") {
+                let newCycle = Cycle(context: moc)
+                newCycle.uuid = UUID()
+                newCycle.startDate = Date()
+                
+                if let expectedEndDate = Calendar.current.date(byAdding: .hour, value: 15, to: Date()) {
+                    newCycle.expectedEndDate = expectedEndDate
+                }
+                print(newCycle)
+
+                do {
+                    try moc.save()
+                } catch {
+                    print("Error saving managed object context: \(error)")
+                }
+            }
+            .background(Color(red: 1, green: 0.75, blue: 0))
+            .foregroundStyle(.white)
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.roundedRectangle)
+            .cornerRadius(8)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
